873dfafef088e649fdd1e2d59c1448de4bf7f475
shtig <igor@renetec.io>
2021-01-19 23:13:49 -0800

    Mods for Pyxis display support

--- export-image/01-set-sources/01-run.sh
+++ export-image/01-set-sources/01-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# Done with build-time package installations, remove staging repository
+sed -i '/pyxian-staging/d' "${ROOTFS_DIR}/etc/apt/sources.list.d/pyxian.list"
+
 rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 find "${ROOTFS_DIR}/var/lib/apt/lists/" -type f -delete
 on_chroot << EOF
